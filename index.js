abacbbd0cac7f359d263a470996dfdffe501e5f7
Alberto Valenzuela <avalen28@gmail.com>
2023-01-12 20:31:31 +0100

    adds new name

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function newFunc() {
+function newFunc2() {
   console.log("I'm very new in this mandanguita");
 }
 
-newFunc();
+newFunc2();
 function megaCool() {
   console.log("I might be the coolest ever");
   return 2 + 2;
